f41c13bef61a1a88e7990e9988bc8ac6d5b1b59c
Victor <kanoru3101@gmail.com>
2018-09-20 12:43:35 +0300

    create Calculator

---
+++ src/MathFunctions.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Container, Row, Col, Button } from 'reactstrap';
+
+
+class MathFunctions extends Component{
+    render(){
+        return(
+          <div>
+              <p>Hello</p>
+          </div>
+        );
+    }
+}
